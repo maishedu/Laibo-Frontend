c7f04f2202e01218ade7b208622bf63be080adcc
samwel-chege <samwelchegeh09@gmail.com>
2023-10-04 01:11:12 +0300

    hot fixes

--- src/components/market/market.js
+++ src/components/market/market.js
@@ -8,7 +8,12 @@ import Label from "../Label";
 import {BsFilter} from 'react-icons/bs'
 
 export default function  Market() {
+  const [posts, setPosts] = useState([])
+  const [isFilterVisible, setIsFilterVisible] = useState(false);
 
+  const handleFilterButtonClick = () => {
+    setIsFilterVisible(!isFilterVisible);
+  };
 
 
   const [searchDetails, setSeachDetails] = useState({
@@ -24,9 +29,6 @@ export default function  Market() {
   }
 
   
-
-  const [posts, setPosts] = useState([])
-  
   async function fetchPosts() {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/laibo/api/posts/fetch?limit=20&page=0`);
     try {
@@ -38,7 +40,6 @@ export default function  Market() {
   
       const data = await response.json();
       setPosts(data)
-      console.log(data)
       return data;
     
     } catch (error) {
@@ -60,7 +61,9 @@ export default function  Market() {
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div className="flex flex-col justify-between lg:flex-row">
 
-          <div className="mb-12 lg:mb-0 w-full  ">
+          <div
+          
+           className="mb-12 lg:mb-0 w-full  ">
             <div className=" mb-6">
               <div className="mx-auto mb-10 flex justify-center w-96 sm:text-center">
               <div class="relative">
@@ -69,17 +72,64 @@ export default function  Market() {
                           <path d="M21 21L15 15M17 10C17 13.866 13.866 17 10 17C6.13401 17 3 13.866 3 10C3 6.13401 6.13401 3 10 3C13.866 3 17 6.13401 17 10Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                       </svg>
                   </span>
-                  <span class="absolute inset-y-0 right-0 flex lg:hidden items-center pr-3">
+                  <span
+                   onClick={handleFilterButtonClick}
+                   class="absolute inset-y-0 right-0 flex lg:hidden items-center pr-3">
+
                     <BsFilter className="w-5 h-5 text-gray-400" />
                   </span>
 
                   <input type="text" class="w-full py-2 pl-10 pr-4 text-gray-300 text-center bg-neutral-700  rounded-md " placeholder="Search"/>
               </div>
-              
-               
               </div>
 
-              <div className="grid gap-5 mx-auto sm:grid-cols-2 lg:grid-cols-4 lg:max-w-screen-lg">
+            {isFilterVisible && (
+             <div className="lg:hidden px-5 pt-6 pb-5 rounded sticky ">
+             <Label>Book Type</Label>
+              <div className="mb-3 rounded-xl bg-neutral-800 ">
+              <Select className="mt-1.5 w-full bg-neutral-800 px-3 py-3 text-white rounded-lg" name="bookType" value={searchDetails.bookType} onChange={handleValueChange}>
+                <option value="E-book">E-book</option>
+                <option value="Hardcover">Hardcover</option>
+              </Select>
+              </div>
+  
+              <Label>Condition</Label>
+                <div className="mb-3 rounded-xl bg-neutral-800 ">
+                <Select className="mt-1.5 bg-neutral-800 px-3 py-3 text-white rounded-lg" name="condition" value={searchDetails.condition} onChange={handleValueChange}>
+                  <option value="Brand new"  >Brand new</option>
+                  <option value="New">New</option>
+                  <option value="Good">Good</option>
+                  <option value="Ok">Ok</option>
+                  <option value="Bad">Bad</option>
+                  <option value="Used">Used</option>
+                  <option value="very-bad">Very bad</option>
+                </Select>
+              </div>
+  
+              <Label>Location</Label>
+                <div className="mb-3 rounded-lg bg-neutral-800 ">
+                  <input placeholder="Location" name="location" value={searchDetails.location} onChange={handleValueChange} className="text-white rounded-lg p-2 bg-neutral-800 w-full"  />
+               
+              </div>
+                 <Label>Price</Label>
+                 <div className="flex justify-center mb-3 space-x-3">
+                  <input placeholder="Min price" name="minPrice" value={searchDetails.minPrice} className="bg-neutral-800 w-full rounded-lg text-white p-2" onChange={handleValueChange}/>
+                  <input placeholder="Max price" name="maxPrice" value={searchDetails.maxPrice} className="bg-neutral-800 w-full rounded-lg text-white p-2" onChange={handleValueChange}/>
+                 </div>
+                
+              <div className="mb-3 text-center rounded-xl default-yellow-bg px-3 py-2">
+                <Link  href={`/market/search/?bookType=${searchDetails.bookType}&condition=${searchDetails.condition}&location=${searchDetails.location}&maxPrice=${searchDetails.maxPrice}&minPrice=${searchDetails.minPrice}`} >
+                 <button type="submit" className="rounded-lg text-center">Show results </button>
+                </Link>
+              
+              </div>
+            
+            </div>
+
+          )}
+
+              <div
+               className="grid gap-5 mx-auto sm:grid-cols-2 lg:grid-cols-4 lg:max-w-screen-lg">
                 {posts.data?.map((post,index) =>(
                    <div key={index} >
                     <Link href={`/market/${post.post_id}`}>
@@ -111,50 +161,55 @@ export default function  Market() {
                 ))}
 
               </div>
+
+
             </div>
           </div>
 
-          <div className="px-5 pt-6 pb-5 rounded sticky">
-           <Label>Book Type</Label>
-            <div className="mb-3 rounded-xl bg-neutral-800 ">
-            <Select className="mt-1.5 w-full bg-neutral-800 px-3 py-3 text-white rounded-lg" name="bookType" value={searchDetails.bookType} onChange={handleValueChange}>
-              <option value="E-book"  >E-book</option>
-              <option value="Hardcover"  >Hardcover</option>
-            </Select>
-            </div>
+      
 
-            <Label>Condition</Label>
+            <div className="hidden lg:block px-5 pt-6 pb-5 rounded sticky ">
+             <Label>Book Type</Label>
               <div className="mb-3 rounded-xl bg-neutral-800 ">
-              <Select className="mt-1.5 bg-neutral-800 px-3 py-3 text-white rounded-lg" name="condition" value={searchDetails.condition} onChange={handleValueChange}>
-                <option value="Brand new"  >Brand new</option>
-                <option value="New">New</option>
-                <option value="Good">Good</option>
-                <option value="Ok">Ok</option>
-                <option value="Bad">Bad</option>
-                <option value="Used">Used</option>
-                <option value="very-bad">Very bad</option>
+              <Select className="mt-1.5 w-full bg-neutral-800 px-3 py-3 text-white rounded-lg" name="bookType" value={searchDetails.bookType} onChange={handleValueChange}>
+                <option value="E-book">E-book</option>
+                <option value="Hardcover">Hardcover</option>
               </Select>
-            </div>
-
-            <Label>Location</Label>
-              <div className="mb-3 rounded-lg bg-neutral-800 ">
-                <input placeholder="Location" name="location" value={searchDetails.location} onChange={handleValueChange} className="text-white rounded-lg p-2 bg-neutral-800 w-full"  />
-             
-            </div>
-               <Label>Price</Label>
-               <div className="flex justify-center mb-3 space-x-3">
-                <input placeholder="Min price" name="minPrice" value={searchDetails.minPrice} className="bg-neutral-800 w-32 rounded-lg text-white p-2" onChange={handleValueChange}/>
-                <input placeholder="Max price" name="maxPrice" value={searchDetails.maxPrice} className="bg-neutral-800 w-32 rounded-lg text-white p-2" onChange={handleValueChange}/>
-               </div>
+              </div>
+  
+              <Label>Condition</Label>
+                <div className="mb-3 rounded-xl bg-neutral-800 ">
+                <Select className="mt-1.5 bg-neutral-800 px-3 py-3 text-white rounded-lg" name="condition" value={searchDetails.condition} onChange={handleValueChange}>
+                  <option value="Brand new"  >Brand new</option>
+                  <option value="New">New</option>
+                  <option value="Good">Good</option>
+                  <option value="Ok">Ok</option>
+                  <option value="Bad">Bad</option>
+                  <option value="Used">Used</option>
+                  <option value="very-bad">Very bad</option>
+                </Select>
+              </div>
+  
+              <Label>Location</Label>
+                <div className="mb-3 rounded-lg bg-neutral-800 ">
+                  <input placeholder="Location" name="location" value={searchDetails.location} onChange={handleValueChange} className="text-white rounded-lg p-2 bg-neutral-800 w-full"  />
+               
+              </div>
+                 <Label>Price</Label>
+                 <div className="flex justify-center mb-3 space-x-3">
+                  <input placeholder="Min price" name="minPrice" value={searchDetails.minPrice} className="bg-neutral-800 w-full rounded-lg text-white p-2" onChange={handleValueChange}/>
+                  <input placeholder="Max price" name="maxPrice" value={searchDetails.maxPrice} className="bg-neutral-800 w-full rounded-lg text-white p-2" onChange={handleValueChange}/>
+                 </div>
+                
+              <div className="mb-3 text-center rounded-xl default-yellow-bg px-3 py-2">
+                <Link  href={`/market/search/?bookType=${searchDetails.bookType}&condition=${searchDetails.condition}&location=${searchDetails.location}&maxPrice=${searchDetails.maxPrice}&minPrice=${searchDetails.minPrice}`} >
+                 <button type="submit" className="rounded-lg text-center">Show results </button>
+                </Link>
               
-            <div className="mb-3 text-center rounded-xl default-yellow-bg px-3 py-2">
-              <Link  href={`/market/search/?bookType=${searchDetails.bookType}&condition=${searchDetails.condition}&location=${searchDetails.location}&maxPrice=${searchDetails.maxPrice}&minPrice=${searchDetails.minPrice}`} >
-               <button type="submit" className=" rounded-lg text-center  ">Show results </button>
-              </Link>
+              </div>
             
             </div>
-          
-          </div>
+
         </div>
       </div>
     </div>
