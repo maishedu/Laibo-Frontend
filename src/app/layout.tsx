6d5f71afbcd66b8260198f08f6536d1420e6500f
maishedu <60256735+maishedu@users.noreply.github.com>
2023-11-13 14:19:22 +0300

    Post detail server side rendering and SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,23 @@ import './globals.css'
 import Footer from '@/components/navigation/Footer'
 import HeaderDefault from '@/components/navigation/HeaderDefault';
 import Provider from "@/components/providers/Provider";
+import { Metadata } from 'next';
+import favicon from '../images/logo4 copy.png';
 
+export const metadata: Metadata = {
+  title: 'Laibo',
+  description: 'Laibo takes book trading from the streets and brings it into a user friendly app where transactions and negotiations between buyers and sellers are made easy and transparent. We give traders live stock valuations, market prices, accounting, receipts and the ability to sell to as many people as possible by sharing links.',
+  icons:{
+      icon:favicon.src,
+      apple:favicon.src
+  }
+}
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  console.log(favicon.src)
   return (
     <html lang="en" >
     <body suppressHydrationWarning={true}>
