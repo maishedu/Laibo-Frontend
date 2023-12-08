786fe5e42d93a65932f4952537288c87757448a4
maishedu <60256735+maishedu@users.noreply.github.com>
2023-12-08 03:12:17 +0300

    Fix make offere and fetch post revalidatw

--- src/app/market/page.tsx
+++ src/app/market/page.tsx
@@ -3,7 +3,8 @@ import React, { Fragment } from "react";
 import Market from '@/components/market/market'
 
 async function fetchPosts() {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/laibo/api/posts/fetch?limit=20&page=1`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/laibo/api/posts/fetch?limit=20&page=1`,
+        { next: { revalidate: 180 } });
     try {
         if (!response.ok) {
             throw new Error('Network response was not ok');
--- src/components/offers/MakeOffer.js
+++ src/components/offers/MakeOffer.js
@@ -98,10 +98,11 @@ const MakeOffer = () => {
           url: photoUrl
         });
       }
+      setShowAddBookModal(false)
     };
-    
 
-  
+
+
     useEffect(() => {
     fetchPost(postId)
       .then((data)=> {
@@ -112,15 +113,13 @@ const MakeOffer = () => {
       })
       fetchUserPosts(page, userId)
       .then((data)=> {
-        setPosts(data.data)
+          setPosts(data)
       })
       .catch((error) =>{
         console.error('Error:', error);
       })
     }, [postId, userId]);
-
-
-  return (
+    return (
     <div className="overflow-hidden py-16 bg-black min-h-screen relative h-2/4">
       <div className="px-4 py-16 mx-auto flex justify-center  md:px-24 lg:px-8 lg:py-20">
       {showAlert && <SnackBar  showAlert={showAlert} alertSeverity={alertSeverity}  setShowAlert={setShowAlert}/>   }
@@ -348,8 +347,8 @@ const MakeOffer = () => {
                 ))}
                   </>
                 ):
-                <div class="flex items-center bg-red-500 text-white text-sm font-bold px-4 py-3" role="alert">
-                  <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/></svg>
+                <div className="flex items-center bg-red-500 text-white text-sm font-bold px-4 py-3" role="alert">
+                  <svg className="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M12.432 0c1.34 0 2.01.912 2.01 1.957 0 1.305-1.164 2.512-2.679 2.512-1.269 0-2.009-.75-1.974-1.99C9.789 1.436 10.67 0 12.432 0zM8.309 20c-1.058 0-1.833-.652-1.093-3.524l1.214-5.092c.211-.814.246-1.141 0-1.141-.317 0-1.689.562-2.502 1.117l-.528-.88c2.572-2.186 5.531-3.467 6.801-3.467 1.057 0 1.233 1.273.705 3.23l-1.391 5.352c-.246.945-.141 1.271.106 1.271.317 0 1.357-.392 2.379-1.207l.6.814C12.098 19.02 9.365 20 8.309 20z"/></svg>
                   <p>You have no books in your stock at the moment.</p>
                 </div>
                 }
