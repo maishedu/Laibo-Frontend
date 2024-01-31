acd0a3919985874f3d82c4387ec23782c4eaa24c
maishedu <60256735+maishedu@users.noreply.github.com>
2024-01-31 13:50:57 +0300

    env change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import React, { Fragment } from "react";
 import Market from '@/components/market/market'
 
 async function fetchPosts() {
+    console.log(process.env.NEXT_PUBLIC_API_URL);
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/laibo/api/posts/fetch?limit=20&page=1`,
         { next: { revalidate: 180 } });
     try {
